269acd987f8d58415635f3a76add921c92484eb4
Elnathan Erh <elnathan.erh@gmail.com>
2017-06-13 16:04:57 +0800

    amended app.js renamed index.js to routes.js

--- app.js
+++ app.js
@@ -1,56 +1,43 @@
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import Debug from 'debug';
-import express from 'express';
-// import logger from 'morgan';
-// import favicon from 'serve-favicon';
-import path from 'path';
-import lessMiddleware from 'less-middleware';
-import index from './routes/index';
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
-const app = express();
-const debug = Debug('2nd-proj-web-app:app');
+// Init app
+var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// Connect with Mongo DB
+mongoose.connect('mongodb://localhost/node-template');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
+// Init middel-ware
 app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// View Engine
+app.set( 'views', path.join(__dirname, 'views'));
+app.set( 'view engine', 'pug');
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Setup sessions
+app.use(session( { secret: 'ilovevdi'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Setup local-strategy
+require('./config/passport')(passport);
+
+// Routes
+require('./routes/routes')(app, passport);
+
+// listen
+app.listen( 3000, function(){
+    console.log('lisning on port 3000');
 });
-
-// error handler
-/* eslint no-unused-vars: 0 */
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-// Handle uncaughtException
-process.on('uncaughtException', (err) => {
-  debug('Caught exception: %j', err);
-  process.exit(1);
-});
-
-export default app;
