620322105c55fc5f32b50081e3932e24087a003c
Elnathan Erh <elnathan.erh@gmail.com>
2017-06-16 12:05:12 +0800

    create procfile, fixed localhost with MongoStore session

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config({silent: true})
 var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
@@ -7,6 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var lessMiddleware = require('less-middleware');
+var MongoStore = require('connect-mongo')(session);
 
 
 //lets require/import the mongodb native drivers.
@@ -19,26 +21,14 @@ var app = express();
 console.log(process.env.MONGODB_URI);
 
 // Connect with Mongo DB
-mongoose.connect(process.env.MONGODB_URI || url, function(err,db) {
+mongoose.connect(process.env.MONGODB_URI, function(err,db) {
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
     console.log('Connection established to', process.env.MONGODB_URI);
   }
 });
-
-// Init middel-ware
-app.use(lessMiddleware(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// View Engine
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+mongoose.Promise = global.Promise;
 
 // Setup sessions
 app.use(cookieParser(process.env.SESSION_SECRET));
@@ -61,6 +51,19 @@ require('./config/passport')(passport);
 
 // Routes
 require('./routes/routes')(app, passport);
+
+// Init middel-ware
+app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View Engine
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // listen
 app.listen(3000, function(){
