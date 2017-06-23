b2415aeb70725eae191fde2ef38aef5bf8abb109
Elnathan Erh <elnathan.erh@gmail.com>
2017-06-23 15:48:34 +0800

    minor changes to ppt

--- routes/index.js
+++ routes/index.js
@@ -9,25 +9,25 @@ router.post('/', homeController.login);  //   /
 
 // Facebook Login, get scope from facebook user
 router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
-router.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/index' }), (req, res) => {
+router.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }), (req, res) => {
           res.redirect(req.session.returnTo || '/secret')
         });
 
 // Twitter Login
 router.get('/auth/twitter', passport.authenticate('twitter'));
-router.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/index' }), (req, res) => {
+router.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/' }), (req, res) => {
         res.redirect(req.session.returnTo || '/secret');
         });
 
 // Instagram Login
 router.get('/auth/instagram', passport.authenticate('instagram'));
-router.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/index' }), (req, res) => {
+router.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/' }), (req, res) => {
           res.redirect(req.session.returnTo || '/secret');
         });
 
 // Google Login
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
-router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/index' }), (req, res) => {
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
           res.redirect(req.session.returnTo || '/secret');
         });
 
