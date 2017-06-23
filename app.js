a7c9fec2b72e267605b6903016ca20ad49a66adc
Elnathan Erh <elnathan.erh@gmail.com>
2017-06-23 10:15:25 +0800

    prepare for deployment on heroku

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const app = express();
 const debug = Debug('2nd-proj-web-app:app');
 
 // Connect with Mongo DB
-mongoose.connect(localMongoURL, function(err,db) {
+mongoose.connect(process.env.MONGODB_URI, function(err,db) {
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
